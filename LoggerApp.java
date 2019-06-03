9317d78d92b2cd0a98906f215e06059e4bceec43
Aleksandra Ponochovna <47608387+AleksandraPonochovna@users.noreply.github.com>
2019-06-03 17:53:55 +0300

    Update LoggerApp.java

--- LoggerApp.java
+++ LoggerApp.java
@@ -12,11 +12,11 @@ public class LoggerApp {
     }
 
     private static void configureLogging() {
-        final Logger logClassA = Logger.getLogger("org.stepic.java.logging.ClassA");
+        Logger logClassA = Logger.getLogger("org.stepic.java.logging.ClassA");
         logClassA.setLevel(Level.ALL);
-        final Logger logClassB = Logger.getLogger("org.stepic.java.logging.ClassB");
+        Logger logClassB = Logger.getLogger("org.stepic.java.logging.ClassB");
         logClassB.setLevel(Level.WARNING);
-        final Logger logger = Logger.getLogger("org.stepic.java");
+        Logger logger = Logger.getLogger("org.stepic.java");
         ConsoleHandler consoleHandler = new ConsoleHandler();
         consoleHandler.setLevel(Level.ALL);
         consoleHandler.setFormatter(new XMLFormatter());
